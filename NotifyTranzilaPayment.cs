30d5befd8eda5696100062db06c45416c613d7a8
Yair Cohen <mysmallfish@gmail.com>
2020-02-06 08:01:50 +0200

    adding error details to response  bug fix

--- NotifyTranzilaPayment.cs
+++ NotifyTranzilaPayment.cs
@@ -31,7 +31,9 @@ namespace Redox.Payments
                 .Build();
             var json =
                 "{\"BillingId\":444,\"Items\":[{\"Description\":\"ברוקר מורחב\",\"Sku\":\"14\",\"Price\":49.0,\"Quantity\":2}],\"Account\":{\"Id\":152,\"Name\":\"בייס אחזקות בע\\\"מ\",\"TaxId\":\"514792456\",\"Tenant\":null,\"User\":null},\"Tenant\":{\"Id\":3655,\"Name\":\"מפתח העיר אריאל\",\"Address\":\"אורי בראון 6\",\"City\":\"אריאל\",\"AdminEmail\":\"elikob15@gmail.com\"},\"User\":{\"ManagementRegion\":null,\"CountDealsTotal\":0,\"CountExelosiveTotal\":0,\"CountSignTotal\":0,\"UserId\":3712,\"FullName\":\"אליקו בן עיון\",\"Email\":\"elikob15@gmail.com\",\"Phone\":\"0544595821\"},\"Description\":\"מנוי REDOX - החתמה דיגיטלית למתווכים\",\"Payment\":{\"PaymentDate\":\"2020-02-06T01:25:08.59Z\",\"Amount\":115.0,\"Agents\":2,\"CardNum\":\"6255\"}}";
-            await InvoiceCustomer.ProcessInvoice(json, log,config );
+            json =
+                "{\"BillingId\":445,\"Items\":[{\"Description\":\"סוכן במשרד\",\"Sku\":\"4\",\"Price\":39.0,\"Quantity\":3}],\"Account\":{\"Id\":124,\"Name\":\"הומניר נכסים\",\"TaxId\":\"038719944\",\"Tenant\":null,\"User\":null},\"Tenant\":{\"Id\":4004,\"Name\":\"הומניר נכסים\",\"Address\":\"מצדה 7 , בניין ב.ס.ר 4 קומה 24\",\"City\":\"בני ברק\",\"AdminEmail\":\"office@homenir.co.il\"},\"User\":{\"ManagementRegion\":null,\"CountDealsTotal\":0,\"CountExelosiveTotal\":0,\"CountSignTotal\":0,\"UserId\":4099,\"FullName\":\"דרור ניר\",\"Email\":\"office@homenir.co.il\",\"Phone\":\"0548038317\"},\"Description\":\"מנוי REDOX - החתמה דיגיטלית למתווכים\",\"Payment\":{\"PaymentDate\":\"2020-02-06T01:35:09.2Z\",\"Amount\":137.0,\"Agents\":4,\"CardNum\":\"6407\"}}";
+           // await InvoiceCustomer.ProcessInvoice(json, log,config );
             log.LogInformation("C# HTTP trigger function processed a request.");
             var items = new StringBuilder();
             var properties = new Dictionary<string,string>();
